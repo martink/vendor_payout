68a25a980cb2c467f1b108cf0c0405ca38a51a7d
Martin Kamerbeek <martin@oqapi.nl>
2009-02-27 12:48:06 +0000

    Finished test data generator

--- sbin/createTestData.pl
+++ sbin/createTestData.pl
@@ -24,6 +24,10 @@ use Getopt::Long;
 use WebGUI::Session;
 use WebGUI::User;
 use WebGUI::Shop::Vendor;
+use WebGUI::Shop::Cart;
+use WebGUI::Shop::Transaction;
+use WebGUI::Shop::Address;
+use WebGUI::Shop::AddressBook;
 
 # Get parameters here, including $help
 GetOptions(
@@ -32,9 +36,85 @@ GetOptions(
 
 my $session = start( $webguiRoot, $configFile );
 
-my $vendors = createVendors( $session );
+$session->setting->set( 'versionTagMode', 'multiPerUser' );
+
+my $vendors         = createVendors( $session );
+my $products        = createProducts( $session, $vendors );
+my $transactions    = createTransactions( $session, $products );
+
 
 finish($session);
+
+#----------------------------------------------------------------------------
+sub createTransactions {
+    my $session     = shift;
+    my $products    = shift;
+    my $productCount = scalar @{ $products };
+
+    # Create a dummy address.
+    my $addressBook = WebGUI::Shop::AddressBook->newBySession( $session );
+    my $address = WebGUI::Shop::Address->create( $addressBook, {} );
+
+    for my $i (1..50) {
+        print "Setting up a new cart...\n";
+        my $cart = WebGUI::Shop::Cart->create( $session );
+        $cart->update( { 
+            shippingAddressId   => $address->getId,
+            shipperId           => 'defaultfreeshipping000',
+        } );
+        
+        for (1..int(rand(6))) {
+            my $sku = $products->[ int rand $productCount ];
+            my $qty = int rand(4) + 1;
+
+            print "\tAdding $qty item(s) ".$sku->getId ." -> ".$sku->get('title')."\n";
+            my $item = $cart->addItem( $sku );
+#            $item->setQuantity( $qty );
+        }
+
+        print "\tCreating transaction\n";
+        my $transaction = WebGUI::Shop::Transaction->create( $session, { cart => $cart } );
+        $transaction->completePurchase( "test transaction $i", 1, 'OK' );
+    }
+
+}
+
+#----------------------------------------------------------------------------
+sub createProducts {
+    my $session = shift;
+    my $vendors = shift;
+    my @bazaarItems;
+
+    # First create a bazaar
+    print "Adding bazaar\n";
+    my $bazaar = WebGUI::Asset->getImportNode( $session )->addChild( {
+        className   => 'WebGUI::Asset::Wobject::Bazaar',
+        title       => 'Vendor payout test bazaar',
+        url         => 'testbazaar',
+    } );
+
+    # Create bazaar items
+    for my $i (1..20) {
+        my $vendor = $vendors->[ int rand( scalar @$vendors ) ];
+
+        my $properties = {
+            className               => 'WebGUI::Asset::Sku::BazaarItem',
+            title                   => "Test bazaar item $i",
+            price                   => $i < 6 ? 0 : $i,
+            vendorPayoutPercentage  => $i % 2 ? int rand(101) : 0,
+            vendorId                => $vendor->get('vendorId'),
+        };
+
+        print "\tAdding bazaar item: $properties->{title}, price: $properties->{price}, "
+            ."vpp: $properties->{vendorPayoutPercentage} %, vendor: ".$vendor->get('name')."\n";
+        my $item = $bazaar->addChild( $properties );
+        $item->requestAutoCommit;
+        push @bazaarItems, $item;
+    }
+
+    return \@bazaarItems;
+}
+
 
 #----------------------------------------------------------------------------
 sub createVendors {
@@ -74,12 +154,12 @@ sub start {
     my $session = WebGUI::Session->open($webguiRoot,$configFile);
     $session->user({userId=>3});
     
-    ## If your script is adding or changing content you need these lines, otherwise leave them commented
+    # If your script is adding or changing content you need these lines, otherwise leave them commented
+    
+     my $versionTag = WebGUI::VersionTag->getWorking($session);
+     $versionTag->set({name => 'Name Your Tag'});
+    
     #
-    # my $versionTag = WebGUI::VersionTag->getWorking($session);
-    # $versionTag->set({name => 'Name Your Tag'});
-    #
-    ##
     
     return $session;
 }
@@ -89,9 +169,9 @@ sub finish {
     my $session = shift;
     
     ## If your script is adding or changing content you need these lines, otherwise leave them commented
-    #
-    # my $versionTag = WebGUI::VersionTag->getWorking($session);
-    # $versionTag->commit;
+    
+     my $versionTag = WebGUI::VersionTag->getWorking($session);
+     $versionTag->commit;
     ##
     
     $session->var->end;
